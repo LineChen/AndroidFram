7bb4b561914fb5c4419edcf564bdb5c843694cc2
Beiing <15764230067@163.com>
2016-04-21 15:37:05 +0800

    add common adapter for listview or gridview

---
+++ baseframe/src/main/java/com/beiing/baseframe/adapter/forlistview/MultiItemTypeSupport.java
@@ -0,0 +1,10 @@
+package com.beiing.baseframe.adapter.forlistview;
+
+public interface MultiItemTypeSupport<T>
+{
+	int getLayoutId(int position, T t);
+
+	int getViewTypeCount();
+
+	int getItemViewType(int position, T t);
+}
